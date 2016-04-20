4f4ceede93942f83d757a0b07d184aaf48ad36cf
T.A. de Jong <tadj42@gmail.com>
2016-04-20 17:03:05 +0200

    Updated variables for better simulation-

--- wolff_multi_chi.py
+++ wolff_multi_chi.py
@@ -5,7 +5,7 @@ import time
 import __builtin__ as std
 
 d = 2
-Beta = arange(0.42,0.44,0.0005)
+Beta = arange(0.425,0.445,0.0005)
 
 def nbr (x,todo) :
     for i in range(d) : 
@@ -22,6 +22,7 @@ def hexnbr (x,todo) :
 
 def run (beta) :
     t = time.clock()
+	#Total magnetization
     M = zeros(steps)
     random.seed()
     p = current_process()
@@ -46,16 +47,18 @@ def run (beta) :
     t = time.clock() - t
     print 'beta =', beta, ', n =', n , 'data generated in', t, 'at', p.name
     M = array(M[grootte:steps])
+	#Chi = susceptibility per spin
     Chi = beta*(mean(M**2,0)-mean(absolute(M),0)**2)/grootte
+	#Mabs absolute value of the mean magnetization per spin
     Mabs = mean(absolute(M),0)/grootte
     return (Chi,Mabs,t)
 name = 'wolffzoom'
 if __name__ == '__main__':
-    for n in [100] :
+    for n in [20, 30, 40, 50, 60, 120] :
         Chi = []
         Mabs = []
         t = []
-        steps = 12000
+        steps = 15000
         grootte = n**d
         p = Pool()
         #args = map(lambda b: (grootte, steps, b), beta)
