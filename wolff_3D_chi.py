5d27389c6b9589caa10fba996010f34fc9529eb5
T.A. de Jong <tadj42@gmail.com>
2016-05-16 18:01:14 +0200

    Further cleanup & added hcp neughbours

--- wolff_3D_chi.py
+++ wolff_3D_chi.py
@@ -7,18 +7,21 @@ import __builtin__ as std
 d = 3
 Beta = arange(0.1,0.3,0.005)
 
-def nbr (x,todo,nbrdelta) :
-    #for i in range(d) : 
-    #    todo.append((x+ n**i)%grootte)
-    #    todo.append((x- n**i)%grootte)
-    todo.extend([(x+i)%grootte for i in nbrdelta])
+def nbr (n,d) :
+    nbrs = []
+    for i in range(d) :
+        nbrs+= [n**i,-n**i]
+    return nbrs
 
-def hexnbr (x,todo) :
-    for i in range(d) : 
-        todo.append((x+ n**i)%grootte)
-        todo.append((x- n**i)%grootte)
-    todo.append((x+ n-1)%grootte)
-    todo.append((x- n+1)%grootte)
+def hexnbr (n,d) :
+    nbrs = []
+    for i in range(d) :
+        nbrs+= [n**i,-n**i]
+    nbrs+=[n-1,-n+1]
+    if d == 3 :
+        nbrs += [n**2-1,-n**2+1]
+        nbrs += [n**2-n,-n**2+n]
+    return nbrs
 
 
 def run (beta) :
@@ -30,9 +33,7 @@ def run (beta) :
     spins = random.randint(0,2,grootte)
     M[0] = 2.0*sum(spins)-grootte
     randloc = random.randint(0,grootte,steps)
-    nbrdelta =[]
-    for i in range(d) :
-        nbrdelta += [n**i,-n**i]
+    nbrdelta = nbr(n,d)
     Padd = 1-exp(-2*beta)
     for i in range(steps-1) :
         x = randloc[i]
